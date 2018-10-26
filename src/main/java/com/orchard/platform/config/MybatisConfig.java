69939ab9a5741bfe4546c0441f4081289859c32d
orchard.chang@dji.com <orchard.chang@dji.com>
2018-10-27 00:11:14 +0800

    很多东西

--- src/main/java/com/orchard/platform/config/MybatisConfig.java
+++ src/main/java/com/orchard/platform/config/MybatisConfig.java
@@ -1,10 +1,13 @@
 package com.orchard.platform.config;
 
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -16,10 +19,10 @@ import javax.sql.DataSource;
 @MapperScan("com.orchard.platform.dao.mapper")
 
 public class MybatisConfig {
-    @Bean
-    public DataSource dataSource() {
-        return DruidDataSourceBuilder.create().build();
-    }
+    //@Bean
+    //public DataSource dataSource() {
+    //    return DruidDataSourceBuilder.create().build();
+    //}
 
     @Bean
     public SqlSessionFactory sqlSessionFactory() throws Exception {
@@ -28,5 +31,21 @@ public class MybatisConfig {
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         bean.setMapperLocations(resolver.getResources("com.orchard.platform.dao.mapper/*Mapper.xml"));
         return bean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory());
+    }
+
+    //@Bean
+    //public Interceptor pageHelper() {
+    //    PageIntercepts pageHelper = new PageIntercepts(dialect);
+    //    return pageHelper;
+    //}
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 }
